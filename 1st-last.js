8c47c9b04c63eb7a200e4d2a17ac5afe5dc6ae54
Madhumita Dasgupta <maccrin@gmail.com>
2025-06-18 21:10:01 +0200

    1st and last index found

--- 1st-last.js
+++ 1st-last.js
@@ -3,24 +3,45 @@ function searchRange(nums, target) {
     // Write your code here
     let l=0;
     let r=nums.length-1;
-    let mid=Math.floor((l + r) / 2);
 let res=[-1,-1];
+
+//left boundary search
     while(l<=r){
-       if(target>=nums[mid]){
-l=mid;
-res=[l,r]
-       }
-
-       if(target<=nums[mid]){
-r=mid-1;
-res=[l,r];
-       } 
-       mid=Math.ceil(l+r)/2;
+        let mid=Math.floor(l+(r-l)/2);
+     if(target===nums[mid]){
+while(mid >0 && nums[mid]===nums[mid-1])mid--
+res[0]=mid;
+break;
+     }
+     else if( target< nums[mid]){
+       r=mid-1;
+     }
+     else if( target > nums[mid]){
+       l=mid+1;
+     }    
 }
 
- return res;
+//right boundary search
+r=nums.length-1;
+l=0;
+  while(l<=r){
+        let mid=Math.floor(l+(r-l)/2);
+     if(target===nums[mid]){
+while( mid< nums.length-1 && nums[mid]===nums[mid+1]) mid++;
+res[1]=mid;
+break;
+
+     }
+     else if( target>nums[mid]){
+       l=mid+1;
+     }
+      else if( target < nums[mid]){
+       r=mid-1;
+     }    
 }
-console.log(searchRange([5,7,7,8,8,10],8));
+return res;
+}
+console.log(searchRange([5,7,7,8,8,18],8));
 console.log(searchRange([5, 7,7,8,8,10],6));
 console.log(searchRange([1],1));
 console.log(searchRange([1,2,3,4,5,6],4));
