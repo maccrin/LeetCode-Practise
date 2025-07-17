81e2f28849038f68db100bf0037ea673ca38753c
Madhumita Dasgupta <maccrin@gmail.com>
2025-07-17 11:42:25 +0200

    k-sumSubArray-hashMaP

--- k-sumSubArray.js
+++ k-sumSubArray.js
@@ -2,15 +2,28 @@ function subarraySum(nums, k) {
     // Implement your logic here
     //build prefix sum array
     let count=0;
+     let sum=0;
+    let prfxSum={0:1};
     for(let i=0;i<nums.length;i++){
-        let sum=0;
-for(let j=i;j<nums.length;j++){
-   sum+=nums[j];
-    if(sum===k)count++
-}
+       sum+=nums[i];
+       if(prfxSum[sum-k]){
+count++;
+       }
+prfxSum[sum]=(prfxSum[sum]||0)+1;
     }
+    //for(let i=0;i<nums.length;i++){
+       // let sum=0;
+//for(let j=i;j<nums.length;j++){
+  // sum+=nums[j];
+   // if(sum===k)count++
+//}
+    //}
+
+    //usimg hashMap
     return count; // placeholder
 }
+
+
 console.log(subarraySum([1,0,1,0],1));
 console.log(subarraySum([1,1,1],2));
 console.log(subarraySum([1, 2, 3],3));
